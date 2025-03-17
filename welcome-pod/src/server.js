8f881337e80ed908442b158b2a267d0abbf90306
Anis Sofea Hadzlan <126544122+anishdzln@users.noreply.github.com>
2025-03-18 02:50:14 +0800

    Update server.js

--- welcome-pod/src/server.js
+++ welcome-pod/src/server.js
@@ -48,6 +48,6 @@ app.get('/api/bookings/:bookingNumber', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening at http://0.0.0.0:${port}`);
 });
